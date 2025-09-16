1142ccdcb8a420362ecbcd372f2414bf5e5d85bf
CaioPQD <caiomacielpqd@gmail.com>
2025-09-16 17:05:01 -0300

    Adicionando .gitignore e limpando arquivos desnecessários

---
+++ ProjetoPoo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ProjetoPoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine($"Área de X = {areaX.ToString("F4")}");
+            Console.WriteLine($"Área de Y = {areaY.ToString("F4")}");
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Área de X é maior");
+            }
+            else
+            {
+                Console.WriteLine("Área de Y é maior");
+            }
+
+
+
+        }
+    }
+}
